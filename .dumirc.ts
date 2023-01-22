9afa56506e11db291a2d79bcd3d3d20e1396e41b
Tosuke-sama <91041842+Tosuke-sama@users.noreply.github.com>
2023-01-22 16:58:49 +0800

    feat:更新

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'Youmi.github.io';
 export default defineConfig({
+   title: 'Youmi.github.io',
+  mode: 'site',
+  devServer: {
+    port: 1998 // 自定义端口号
+  },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   themeConfig: {
     name: 'Youmi',
   },
