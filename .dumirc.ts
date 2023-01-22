817529d0e15a6f16970263a811aec211d9a51ffe
Tosuke <1848228664@qq.com>
2023-01-22 17:43:06 +0800

    feat: 第五次测试更新

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'dumi';
 
 const repo = 'Youmi.github.io';
 export default defineConfig({
+  base: '/Youmi.github.io',
+  publicPath: '/Youmi.github.io',
   themeConfig: {
     name: 'Youmi',
   },
